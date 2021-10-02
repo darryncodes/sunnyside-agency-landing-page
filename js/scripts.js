de8d878ef0cf6b84a89cc629329d603e26760201
Darryn Codes <hello@darryncodes.co.uk>
2021-10-02 19:06:56 +0100

    creating intro styling and btn component styling

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// 'use strict';
+
+// function toggle(social) {
+//   let popup = document.getElementById('social');
+//   if (popup.style.display == 'flex') popup.style.display = 'none';
+//   else popup.style.display = 'flex';
+// }
+
+// onclick="toggle(social);
